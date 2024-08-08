b189d381609b458d5029d4beb9b77935df5a7dd0
phamducbinhh <phamducbinh1712000@gmail.com>
2024-08-08 10:45:02 +0700

    Update

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,12 +3,16 @@ import { cookies } from "next/headers";
 
 export async function POST(request: Request) {
   const cookieStore = cookies();
+
   const accessToken: any = cookieStore.get("accessToken")?.value;
+
   const refreshToken = cookieStore.get("refreshToken")?.value;
 
   if (!accessToken || !refreshToken) {
     cookieStore.delete("accessToken");
+
     cookieStore.delete("refreshToken");
+
     return Response.json(
       {
         message:
@@ -26,12 +30,14 @@ export async function POST(request: Request) {
       refreshToken,
     });
 
-    console.log('response',response);
-
     return Response.json(response.payload);
+
   } catch (error) {
+    
     cookieStore.delete("accessToken");
+
     cookieStore.delete("refreshToken");
+
     return Response.json(
       {
         message: "Lỗi khi gọi API đến server backend, buộc phải xóa cookie",
