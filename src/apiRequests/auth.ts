7c8f5629dba80a7777b5d8c88092a81e0aab2f88
phamducbinhh <phamducbinh1712000@gmail.com>
2024-08-08 10:44:35 +0700

    Code route handler logout

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -1,12 +1,34 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+import { LoginBodyType, LoginResType, LogoutBodyType } from "@/schemaValidations/auth.schema";
 
 class AuthApiRequest {
   public SLogin(body: LoginBodyType): Promise<any> {
     return http.post<LoginResType>("/auth/login", body);
   }
+
   public login(body: LoginBodyType): Promise<any> {
     return http.post<LoginResType>("/api/auth/login", body, { baseUrl: "" });
+  }
+
+  public SLogout(
+    body: LogoutBodyType & {
+      accessToken: string;
+    }
+  ): Promise<any> {
+    return http.post(
+      "/auth/logout",
+      {
+        refreshToken: body.accessToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+        },
+      }
+    );
+  }
+  public logout(): Promise<any> {
+    return http.post<LoginResType>("/api/auth/logout", null, { baseUrl: "" });
   }
 }
 
---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,44 @@
+import authApiRequest from "@/apiRequests/auth";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const cookieStore = cookies();
+  const accessToken: any = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+
+  if (!accessToken || !refreshToken) {
+    cookieStore.delete("accessToken");
+    cookieStore.delete("refreshToken");
+    return Response.json(
+      {
+        message:
+          "Không nhận được access token hoặc refresh token, buộc phải xóa cookie",
+      },
+      {
+        status: 200,
+      }
+    );
+  }
+
+  try {
+    const response = await authApiRequest.SLogout({
+      accessToken,
+      refreshToken,
+    });
+
+    console.log('response',response);
+
+    return Response.json(response.payload);
+  } catch (error) {
+    cookieStore.delete("accessToken");
+    cookieStore.delete("refreshToken");
+    return Response.json(
+      {
+        message: "Lỗi khi gọi API đến server backend, buộc phải xóa cookie",
+      },
+      {
+        status: 200,
+      }
+    );
+  }
+}
